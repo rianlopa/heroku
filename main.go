faa44850c6487c6e91e38fe222df43ad7673773d
rianlopa <rianlopa@hotmail.com>
2019-03-25 01:23:07 -0500

    Update main.go

--- main.go
+++ main.go
@@ -4,12 +4,24 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"database/sql"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	
+	db, err := sql.Open("postgres", os.Getenv("postgresql-infinite-10767"))
+  		if err != nil {
+    		log.Fatal(err)
+	}
+	
+	defer db.Close()
+	
+	qr, err := db.Query(`INSERT INTO public."Name"(nombres) VALUES ('Jean');`)
+	
 	port := os.Getenv("PORT")
 
 	if port == "" {
